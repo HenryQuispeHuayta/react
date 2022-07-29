1aa5749b24c16858b94c80806f5efae8189913ab
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-07-29 18:53:48 -0400

    contador de clicks

---
+++ contador_clicks/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+import {useState} from 'react';
+
+function App() {
+  const [numClicks, setNumClicks] = useState(0);
+  const manejarClick = ()=>{
+    setNumClicks(numClicks + 1);
+  }
+  const reiniciarContador = ()=>{
+    setNumClicks(0);
+  }
+
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Log de freeCodeCamp'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClicks={numClicks} />
+         <Boton
+          texto='Clic'
+          esBotonDeClick={true}
+          manejarClick={manejarClick}
+         />
+         <Boton
+          texto='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}
+         />
+      </div>
+    </div>
+  );
+}
+
+export default App;
